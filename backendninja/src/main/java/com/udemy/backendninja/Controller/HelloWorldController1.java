8f5e64f5738c15a544a620b53c7e6f6a5ba80e6c
Josemolamazo <reyarijonjose@gmail.com>
2021-07-17 14:03:28 +0200

    Nueva plantilla, primer tipo peticion GET

---
+++ backendninja/src/main/java/com/udemy/backendninja/Controller/HelloWorldController1.java
@@ -0,0 +1,24 @@
+package com.udemy.backendninja.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/say2")
+public class HelloWorldController1 {
+	
+	private static final String HELLO_VIEW = "example2";
+	
+	//localhost:8080/say2/request1?nm=JON
+	//localhost:8080/say2/request1?nm=MIKEL
+	@GetMapping("/request1")
+	public ModelAndView request1(@RequestParam(name ="nm", required=false, defaultValue = "World") String name) {
+		ModelAndView mav = new ModelAndView(HELLO_VIEW);
+		mav.addObject("nm_in_model", name);
+		
+		return mav;
+	}
+}
