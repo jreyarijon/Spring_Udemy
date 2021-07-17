94150e7a67bf0db07a287d1e557db0a06c103532
Josemolamazo <Jreyarijon@danielcastelao.org>
2021-07-17 15:44:37 +0200

    Segundo tipo de peticiones GET

--- backendninja/src/main/java/com/udemy/backendninja/Controller/HelloWorldController1.java
+++ backendninja/src/main/java/com/udemy/backendninja/Controller/HelloWorldController1.java
@@ -1,7 +1,9 @@
 package com.udemy.backendninja.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +18,17 @@ public class HelloWorldController1 {
 	//localhost:8080/say2/request1?nm=MIKEL
 	@GetMapping("/request1")
 	public ModelAndView request1(@RequestParam(name ="nm", required=false, defaultValue = "World") String name) {
+		ModelAndView mav = new ModelAndView(HELLO_VIEW);
+		mav.addObject("nm_in_model", name);
+		
+		return mav;
+	}
+	
+	//localhost:8080/say2/request2/JON
+	//localhost:8080/say2/request2/MIKEL
+	
+	@GetMapping("request2/{nm}")
+	public ModelAndView request2(@PathVariable("nm") String name) {
 		ModelAndView mav = new ModelAndView(HELLO_VIEW);
 		mav.addObject("nm_in_model", name);
 		
