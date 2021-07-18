517e6972428d5ea8b13718101248a2ed94454b65
Josemolamazo <reyarijonjose@gmail.com>
2021-07-18 14:07:38 +0200

    Metodos de redireccionamiento

--- backendninja/src/main/java/com/udemy/backendninja/Controller/FormController.java
+++ backendninja/src/main/java/com/udemy/backendninja/Controller/FormController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.udemy.backendninja.model.Person;
 
@@ -17,6 +18,20 @@ public class FormController {
 
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
+	
+	//Primera forma de redireccionar
+	/*@GetMapping("/")
+	public String redirect() {
+		
+		return "redirect:/form/showForm";
+	}*/
+	
+	//Segunda forma
+	@GetMapping("/")
+	public RedirectView redirectView() {
+		
+		return new RedirectView("/form/showForm");
+	}
 	
 	@GetMapping("/showForm")
 	public String showForm(Model model) {
