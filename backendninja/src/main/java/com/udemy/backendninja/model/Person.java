c1a069418685673c44dec63fd1f1cc9d2b0a54fd
Josemolamazo <reyarijonjose@gmail.com>
2021-07-18 13:56:45 +0200

    Peticion Post añadida

--- backendninja/src/main/java/com/udemy/backendninja/model/Person.java
+++ backendninja/src/main/java/com/udemy/backendninja/model/Person.java
@@ -4,12 +4,16 @@ public class Person {
 
 	private String name;
 	private int age;
+	
+	public Person() {
+
+	}
 
 	public Person(String name, int age) {
-		super();
 		this.name = name;
 		this.age = age;
 	}
+	
 
 	public String getName() {
 		return name;
